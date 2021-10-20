c1cc6ffc740734bfce68718804c45c2ec1aadd1a
szymon <szymon.popielarz@gmail.com>
2021-10-20 12:22:18 +0200

    pwa added

--- next.config.js
+++ next.config.js
@@ -1,10 +1,25 @@
-/** @type {import('next').NextConfig} */
-
+const withPWA = require('next-pwa');
 const path = require('path');
 
-module.exports = {
+/** @type {import('next').NextConfig} */
+
+module.exports = withPWA({
+  pwa: {
+    dest: 'public',
+    register: true,
+    skipWaiting: true,
+  },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+
+    return config;
+  },
+
   reactStrictMode: true,
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
-};
+});
