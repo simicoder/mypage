c1d164bfa1f8387db6c1c7c2b13da3ef9f6aa07a
szymon <szymon.popielarz@gmail.com>
2021-10-23 10:07:33 +0200

    Header modified

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,6 +1,8 @@
 import { ReactNode, memo } from 'react';
 import Image from 'next/image';
 import styles from './Header.module.scss';
+import stars from './Stars.module.sass';
+import animation from '../../styles/Animation.module.scss';
 
 type HeaderProps = {
   children: ReactNode;
@@ -8,18 +10,24 @@ type HeaderProps = {
 
 export const Header = memo(({ children }) => {
   return (
-    <header className={styles.wrapper}>
-      <Image
-        src="/images/me.png"
-        alt="simicoder"
-        width="300"
-        height="300"
-        className={styles.heroImage}
-        quality={100}
-        priority
-      />
+    <header className={`${styles.wrapper} ${animation.fadeInUp}`}>
+      <div className={stars.starsWrapper}>
+        <div className={stars.stars}></div>
+        <div className={stars.stars2}></div>
+        <div className={stars.stars3}></div>
+        <Image
+          src="/images/me.png"
+          alt="simicoder"
+          width="300"
+          height="300"
+          className={styles.heroImage}
+          quality={100}
+          priority
+        />
+      </div>
+
       <div className={styles.textWrapper}>
-        <p className={styles.lp}>I`m Szymon Popielarz</p>
+        <p className={styles.lp}>I`m Simon Popielarz</p>
         <p className={styles.rp}>I`m a Software Developer</p>
       </div>
     </header>
